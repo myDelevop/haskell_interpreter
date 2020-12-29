fbf0d24ee6f29e8b4ba4a6a2162a3814236d35a5
Rocco Caliandro <r.caliandro@iconsulting.biz>
2020-12-29 23:06:05 +0100

    Added the definition of modifyEnv and updateEnv

--- interpreter.hs
+++ interpreter.hs
@@ -9,18 +9,32 @@ type Env = [Variable]
 newtype Parser a = P (Env -> String -> [(Env, a, String)])
 
 
+-- Update the environment with a variable
+-- If the variable is new (not declared before), it will be
+-- added in the environment 
+-- If the variable exstits, its value will be overwritten in.
 modifyEnv :: Env -> Variable -> Env
 modifyEnv [] var = [var]
 modifyEnv (x:xs) newVar = if (name x) == (name newVar) then [newVar] ++ xs
                         else [x] ++ modifyEnv xs newVar
 
--- Update the environment with a variable
--- If the variable is new (not declared before), it will be
--- added in the environment 
--- If the variable exstits, its value will be overwritten in.
 updateEnv :: Variable -> Parser String
 updateEnv var = P(\env input -> case input of
                     xs -> [((modifyEnv env var), "", xs)])
+
+
+-- Return the value of a variable given the name
+readVariable :: String -> Parser Int
+readVariable name = P (\env input -> case searchVariable env name of
+                                [] -> []
+                                [value] -> [(env, value, input)])
+
+-- Search the value of a variable stored in the Env, given the name
+searchVariable :: Env -> String -> [Int]
+searchVariable [] queryname = []
+searchVariable (x:xs) queryname = 
+                            if (name x) == queryname then [(value x)]
+                            else searchVariable xs queryname
 
 
 main = do 

232ec96897e5a0d2609b6a61695f321689e3ba07
Rocco Caliandro <r.caliandro@iconsulting.biz>
2020-12-29 22:56:45 +0100

    definition of first data types and definition of modifyEnd and updateEnv

--- interpreter.hs
+++ interpreter.hs
@@ -1,1 +1,27 @@
---first commit
+data Variable = Variable {
+    name :: String,
+    vtype :: String,
+    value :: Int
+} deriving Show
+
+type Env = [Variable]
+
+newtype Parser a = P (Env -> String -> [(Env, a, String)])
+
+
+modifyEnv :: Env -> Variable -> Env
+modifyEnv [] var = [var]
+modifyEnv (x:xs) newVar = if (name x) == (name newVar) then [newVar] ++ xs
+                        else [x] ++ modifyEnv xs newVar
+
+-- Update the environment with a variable
+-- If the variable is new (not declared before), it will be
+-- added in the environment 
+-- If the variable exstits, its value will be overwritten in.
+updateEnv :: Variable -> Parser String
+updateEnv var = P(\env input -> case input of
+                    xs -> [((modifyEnv env var), "", xs)])
+
+
+main = do 
+    print("hello")

93144b049cc199010b1d2e24bed8e5c6cdd9b149
Rocco Caliandro <r.caliandro@iconsulting.biz>
2020-12-30 09:03:20 +0100

    Definition of Functor, Applicative and Monad for the Parser

--- interpreter.hs
+++ interpreter.hs
@@ -1,3 +1,5 @@
+import Control.Applicative 
+
 data Variable = Variable {
     name :: String,
     vtype :: String,
@@ -7,6 +9,9 @@ data Variable = Variable {
 type Env = [Variable]
 
 newtype Parser a = P (Env -> String -> [(Env, a, String)])
+
+parse :: Parser a -> Env -> String -> [(Env,a,String)]
+parse (P p) env inp = p env inp
 
 
 -- Update the environment with a variable
@@ -36,6 +41,29 @@ searchVariable (x:xs) queryname =
                             if (name x) == queryname then [(value x)]
                             else searchVariable xs queryname
 
+
+
+instance Functor Parser where
+    -- fmap :: (a->b) -> Parser a -> Parser b
+    fmap g p = P (\env input -> case parse p env input of 
+        [] -> []
+        [(env, v, out)] -> [(env, g v, out)])
+
+
+instance Applicative Parser where 
+    -- pure :: a -> Parser a
+    pure v = P (\env input -> [(env, v, input)])
+    -- <*> :: Parser(a -> b) -> Parser a -> Parser b
+    pg <*> px = P(\env input -> case parse pg env input of 
+        [] -> []
+        [(env, g, out)] -> parse(fmap g px) env out)
+
+
+instance Monad Parser where
+    -- (>>=) :: Parser a -> (a -> Parser b) -> Parser b
+    p >>= f = P (\env input -> case parse p env input of
+        [] -> []
+        [(env, v, out)] -> parse(f v) env out)
 
 main = do 
     print("hello")
